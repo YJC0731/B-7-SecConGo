107f1b01a613fd42f36d6b61dabc92a5d903b573
Lary <zen0718zen@gmail.com>
2024-10-16 00:10:14 +0800

    新增js

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ import './assets/scss/all.scss';
 
 console.log("Hello world!");
 const swiper = new Swiper('.mySwiper', {
-    slidesPerView: 3, // 顯示三個 slide
-    spaceBetween: 10, // Slide 之間的距離
+    slidesPerView: 'auto', // 自動根據 CSS 的寬度調整
+    spaceBetween: -20, // Slide 之間的距離
     centeredSlides: true, // 中心對齊
     loop: true, // 循環播放
-    freeMode: false, // 自由模式
+    freeMode: true, // 自由模式
     pagination: {
         el: '.swiper-pagination', // 分頁點容器
         clickable: true, // 允許點擊分頁點
