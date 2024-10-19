11365355d16e7c134e0f8dd8afa07c138270f9b4
Lary <zen0718zen@gmail.com>
2024-10-20 00:17:39 +0800

    增加第二段swiper功能

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import './assets/scss/all.scss';
 // banner swiper
 const swiper = new Swiper('.mySwiper', {
     slidesPerView: 'auto', // 自動根據 CSS 的寬度調整
-    spaceBetween: -20, // Slide 之間的距離
+    spaceBetween: -16, // Slide 之間的距離
     centeredSlides: true, // 中心對齊
     loop: true, // 循環播放
     freeMode: true, // 自由模式
@@ -32,25 +32,29 @@ document.addEventListener('DOMContentLoaded', function() {
         },
     });
 
-    // 初始化 Co-Chair 區域的 Swiper
-    var coChairSwiper = new Swiper('.coChairSwiper', {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+});
+// co-chair swiper
+const coChairSwiper = new Swiper('.coChairSwiper', {
+    slidesPerView: 3, // 顯示3個slide
+    // spaceBetween: 30, // Slide之間的距離
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// 登入頁面跳轉
+// 在 DOM 完全載入後才添加事件監聽器
+document.addEventListener('DOMContentLoaded', function() {
+    // 登入按鈕監聽
+    document.querySelector('#login button[type="submit"]').addEventListener('click', function(event) {
+        event.preventDefault(); // 防止默認表單提交行為
+        window.location.href = 'memberOrder.html'; // 將頁面跳轉到 memberOrder.html
     });
 });
-// 登入頁面跳轉
-document.getElementById('login-tab').addEventListener('click', function() {
-    window.location.href = 'memberOrder.html'; // 這裡替換為你想跳轉的頁面 URL
-});
+
 // 搜尋框
 document.getElementById('searchButton').addEventListener('click', function() {
     let searchBox = document.getElementById('searchBox');
